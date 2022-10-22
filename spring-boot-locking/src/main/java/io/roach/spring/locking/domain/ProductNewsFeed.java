29249046bae91b53da9cd6e75604da404a4d9270
kai-niemi <kai.niemi.se@gmail.com>
2022-10-22 14:35:27 +0200

    Lock update

--- spring-boot-locking/src/main/java/io/roach/spring/locking/domain/ProductNewsFeed.java
+++ spring-boot-locking/src/main/java/io/roach/spring/locking/domain/ProductNewsFeed.java
@@ -1,5 +1,10 @@
 package io.roach.spring.locking.domain;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,27 +19,36 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 public class ProductNewsFeed {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    private static final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
     @Autowired
     private ProductService productService;
 
-    @Scheduled(cron = "2 * * * * ?")
-    @SchedulerLock(lockAtLeastFor = "1m", lockAtMostFor = "5m", name = "publishNews_clusterSingleton")
+    @Scheduled(cron = "0/15 * * * * ?")
+    @SchedulerLock(lockAtLeastFor = "1m", lockAtMostFor = "3m", name = "publishNews_clusterSingleton")
     public void publishNews() {
         logger.info(">> Entered cluster singleton method");
 
         Page<Product> products = productService.findAll(Pageable.ofSize(128));
         while (products.hasContent()) {
             logger.info("Processing page {} of {}", products.getNumber(), products.getTotalPages());
-
             if (!products.hasNext()) {
                 break;
             }
             products = productService.findAll(products.nextPageable());
         }
 
+        final long startTime = System.currentTimeMillis();
+
+        ScheduledFuture<?> f = executorService.scheduleAtFixedRate(() -> {
+            logger.info("Gone fishing for {}s...", (System.currentTimeMillis() - startTime) / 1000);
+        }, 0, 5, TimeUnit.SECONDS);
+
         try {
-            logger.info("Thinking for 3min..");
-            Thread.sleep(3*60*1_000);
+            // Set this to higher than lockAtMostFor to observe breach of invariant
+            executorService.awaitTermination(2, TimeUnit.MINUTES);
+            f.cancel(true);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
